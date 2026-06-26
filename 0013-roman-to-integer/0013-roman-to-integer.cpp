dd24ffe05ee4ab9f5dd770a4378e4fb945345379
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-06-26 17:24:19 +0530

    Time: 12 ms (17.98%), Space: 13.5 MB (6.75%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    int value(char c) {
-        if (c == 'I') return 1;
-        if (c == 'V') return 5;
-        if (c == 'X') return 10;
-        if (c == 'L') return 50;
-        if (c == 'C') return 100;
-        if (c == 'D') return 500;
-        if (c == 'M') return 1000;
-        return 0;
-    }
-
-    int romanToInt(string s) {
-        int result = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            int curr = value(s[i]);
-            int next = (i + 1 < s.length()) ? value(s[i + 1]) : 0;
-
-            if (curr < next) {
-                result -= curr;
-            }
-            else {
-                result += curr;
-            }
+    int romanToInt(string c) {
+        int d=c.length();
+        int res=0;
+        unordered_map<char,int>roman={
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
+        };
+     for(int i=0;i<d-1;i++){
+        if(roman[c[i]]<roman[c[i+1]]){
+            res-=roman[c[i]];
         }
-
-        return result;
+        else{
+            res+=roman[c[i]];
+        }
+     }
+     return res+ roman[c.back()];
     }
 };
