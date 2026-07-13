dc292b5db6372f8be0832e91715fa97d758402a6
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-07-13 22:38:30 +0530

    Time: 4 ms (77.88%), Space: 11.8 MB (95.68%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool repeatedSubstringPattern(string s) {
+        int a=s.length();
+        for (int len = 1; len <= a / 2; len++) {
+            if (a % len != 0)
+                continue;
+
+            bool flag = true;
+
+            for (int i = len,  j = 0; i < a; i++, j++) {
+
+                if (j == len)
+                    j = 0;
+
+                if (s[i] != s[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag)
+                return true;
+        }
+
+        return false;
+    }
+};
