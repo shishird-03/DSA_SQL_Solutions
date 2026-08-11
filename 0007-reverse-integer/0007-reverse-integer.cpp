957ef122280376a9af3f726eec56081eefcf85c8
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-08-11 23:49:43 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.93%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int answer = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x /= 10;
+            if (answer > INT_MAX / 10 || (answer == INT_MAX / 10 && digit > 7))
+                return 0;
+            if (answer < INT_MIN / 10 ||(answer == INT_MIN / 10 && digit < -8))
+                return 0;
+            answer = answer * 10 + digit;
+        }
+        return answer;
+    }
+};
