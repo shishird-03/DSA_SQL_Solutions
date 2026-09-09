02f80f70d0b22cecd4bbffbb80bf0793a9bb8113
Shishir D <155349832+shishird-03@users.noreply.github.com>
2026-09-09 23:59:59 +0530

    Time: 0 ms (100%), Space: 8.1 MB (79.88%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool detectCapitalUse(string word) {
         int n = word.size();
-
-        // Case 1: all uppercase
         if (word[0] >= 'A' && word[0] <= 'Z') {
             bool allUpper = true;
             for (int i = 1; i < n; i++) {
@@ -12,16 +10,12 @@ public:
             }
 
             if (allUpper) return true;
-
-            // Case 2: first uppercase, rest lowercase
             for (int i = 1; i < n; i++) {
                 if (word[i] >= 'A' && word[i] <= 'Z')
                     return false;
             }
             return true;
         }
-
-        // Case 3: all lowercase
         for (char c : word) {
             if (c >= 'A' && c <= 'Z')
                 return false;
